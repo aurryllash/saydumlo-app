b129a4fd702fc97aa7498a0f80dc8ff7f951d8db
aurryllash <lashatsiklauri77@gmail.com>
2024-05-03 20:50:44 +0400

    add login function and learnt how to protect routes

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 
 const registrationRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
 
 const port = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
+const SECRET = process.env.SECRET
 
 mongoose.connect(DATABASE_URL)
     .then(() => {
@@ -21,13 +24,30 @@ mongoose.connect(DATABASE_URL)
 // EJS
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.json())
+app.use(cookieParser());
 
 app.use('/api', registrationRoutes)
 app.use('/api', loginRoutes)
 
-app.get('/', (req, res) => {
+const authorizationToken = (req, res, next) => {
+    const cookiesToken = req.cookies.token;
+    console.log(cookiesToken)
+    if(!cookiesToken) {
+        return res.sendStatus(403)
+    }
+    jwt.verify(cookiesToken, SECRET, (err, decoded) => {
+        if(err) {
+            return res.sendStatus(403)
+        }
+        console.log(req.user)
+        req.user = decoded
+        console.log(req.user)
+        next();
+    })
+}
+
+app.get('/', authorizationToken, (req, res) => {
     res.render('home')
 })
 
